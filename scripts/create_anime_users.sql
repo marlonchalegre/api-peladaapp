812d83712cf746469e11ee7436cb2efba0608a58
Marlon Chalegre <marlonchalegre@gmail.com>
2025-12-30 15:06:04 -0300

    feat: Add script to seed anime users

---
+++ scripts/create_anime_users.sql
@@ -0,0 +1,29 @@
+-- Script to create 25 anime character users
+-- Password for all: 12345
+
+INSERT OR IGNORE INTO Users (name, email, password) VALUES
+('Son Goku', 'son.goku@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Vegeta', 'vegeta@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Naruto Uzumaki', 'naruto.uzumaki@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Sasuke Uchiha', 'sasuke.uchiha@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Monkey D. Luffy', 'monkey.d.luffy@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Roronoa Zoro', 'roronoa.zoro@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Ichigo Kurosaki', 'ichigo.kurosaki@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Light Yagami', 'light.yagami@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('L Lawliet', 'l.lawliet@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Edward Elric', 'edward.elric@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Alphonse Elric', 'alphonse.elric@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Eren Yeager', 'eren.yeager@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Mikasa Ackerman', 'mikasa.ackerman@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Levi Ackerman', 'levi.ackerman@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Saitama', 'saitama@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Genos', 'genos@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Spike Spiegel', 'spike.spiegel@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Jotaro Kujo', 'jotaro.kujo@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Joseph Joestar', 'joseph.joestar@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Giorno Giovanna', 'giorno.giovanna@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Killua Zoldyck', 'killua.zoldyck@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Gon Freecss', 'gon.freecss@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Sailor Moon', 'sailor.moon@anime.com', 'bcrypt+sha512$9d12ab98308110074.17e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Lelouch Lamperouge', 'lelouch.lamperouge@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103'),
+('Asuka Langley Soryu', 'asuka.langley.soryu@anime.com', 'bcrypt+sha512$9d12ab9830811007417e22693afd43db$12$83d524b936cc0d4111f0c5418b6f73f93f4f179a562b1103');
