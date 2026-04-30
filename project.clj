35496460bb96f921f491b91677e6948196e8b426
Marlon Chalegre <marlonchalegre@gmail.com>
2026-04-30 13:28:13 -0300

    chore(deps): upgrade major and minor dependencies
    - Upgraded buddy-hashers to 2.0.167
    - Upgraded HikariCP to 7.0.2
    - Upgraded ring-mock to 0.6.2
    - Updated Clojure, next.jdbc, sqlite-jdbc, and others to latest stable
    - Optimized namespaces and applied formatting

--- project.clj
+++ project.clj
@@ -3,28 +3,28 @@
   :url "http://example.com/FIXME"
   :license {:name "MIT"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
-                 [org.clojure/data.json "2.5.1"]
+  :dependencies [[org.clojure/clojure "1.12.4"]
+                 [org.clojure/data.json "2.5.2"]
                  [ring/ring "1.13.0"]
                  [buddy/buddy-core "1.12.0-430"]
                  [buddy/buddy-sign "3.6.1-359"]
                  [buddy/buddy-auth "3.0.323"]
-                 [buddy/buddy-hashers "1.4.0"]
+                 [buddy/buddy-hashers "2.0.167"]
                  [ring/ring-json "0.5.1"]
                  [compojure "1.7.2"]
-                 [com.github.seancorfield/next.jdbc "1.3.1086"]
+                 [com.github.seancorfield/next.jdbc "1.3.1093"]
                  [medley/medley "1.4.0"]
-                 [org.xerial/sqlite-jdbc  "3.49.0.0"]
-                 [com.dbeaver.jdbc/com.dbeaver.jdbc.driver.libsql "1.0.5"]
-                 [migratus "1.6.4"]
+                 [org.xerial/sqlite-jdbc  "3.53.0.0"]
+                 [com.dbeaver.jdbc/com.dbeaver.jdbc.driver.libsql "1.0.6"]
+                 [migratus "1.6.5"]
                  [prismatic/schema "1.4.1"]
-                 [com.stuartsierra/component "1.1.0"]
-                 [com.zaxxer/HikariCP  "6.2.1"]
-                 [org.slf4j/slf4j-api "2.0.16"]
-                 [org.slf4j/slf4j-simple "2.0.16"]
-                 [org.clojure/math.combinatorics "0.3.0"]
+                 [com.stuartsierra/component "1.2.0"]
+                 [com.zaxxer/HikariCP  "7.0.2"]
+                 [org.slf4j/slf4j-api "2.0.17"]
+                 [org.slf4j/slf4j-simple "2.0.17"]
+                 [org.clojure/math.combinatorics "0.3.2"]
                  [jarohen/chime "0.3.3"]
-                 [clj-http "3.13.0"]
+                 [clj-http "3.13.1"]
                  [com.draines/postal "2.0.5"]]
   :plugins [[lein-ancient "1.0.0-RC3"]
             [lein-ring "0.12.6"]
@@ -42,12 +42,12 @@
                                   [com.github.clj-kondo/lein-clj-kondo "0.2.5"]]
                    :dependencies [[com.stuartsierra/component.repl "1.0.0"]
                                   [prismatic/schema-generators "0.1.5"]
-                                  [org.clojure/tools.namespace "1.4.4"]
+                                  [org.clojure/tools.namespace "1.5.1"]
                                   [ring/ring-devel "1.13.0"]
-                                  [com.clojure-goes-fast/clj-async-profiler "1.6.1"]]
+                                  [com.clojure-goes-fast/clj-async-profiler "1.7.0"]]
                    :source-paths ["dev"]
                    :repl-options {:init-ns dev}}
-            :test {:dependencies [[ring/ring-mock "0.4.0"]]}}
+            :test {:dependencies [[ring/ring-mock "0.6.2"]]}}
   :ring {:handler api-peladaapp.server/app
          :port 8000
          :reload-paths ["src"]}
--- test/integration/api_peladaapp/scheduler_logic_test.clj
+++ test/integration/api_peladaapp/scheduler_logic_test.clj
@@ -36,32 +36,32 @@
               voter-player-id (db.player/insert-player {:organization-id org-id :user-id voter-user-id :grade 5.0} db)
               target-user-id (db.user/insert-user {:email "target@test.com" :password "pwd" :name "Target" :username "target"} db)
               target-player-id (db.player/insert-player {:organization-id org-id :user-id target-user-id :grade 5.0} db)]
-              
-              ;; 3. Create a Pelada closed 25h ago
-              (let [now (java.time.Instant/now)
-                    old-date (sqlite-date (.minus now (java.time.Duration/ofHours 25)))
-                    pelada-id (db.pelada/insert-pelada {:organization-id org-id :status "closed"} db)]
-                (jdbc/execute! db ["UPDATE Peladas SET status = 'closed', closed_at = ? WHERE id = ?" old-date pelada-id])
 
-                  (jdbc/execute! db ["INSERT INTO peladaattendance (pelada_id, player_id, status, voting_enabled) VALUES (?, ?, 'confirmed', 1)" pelada-id target-player-id])
-                  (jdbc/execute! db ["INSERT INTO peladaattendance (pelada_id, player_id, status, voting_enabled) VALUES (?, ?, 'confirmed', 1)" pelada-id voter-player-id])
-                  
+              ;; 3. Create a Pelada closed 25h ago
+          (let [now (java.time.Instant/now)
+                old-date (sqlite-date (.minus now (java.time.Duration/ofHours 25)))
+                pelada-id (db.pelada/insert-pelada {:organization-id org-id :status "closed"} db)]
+            (jdbc/execute! db ["UPDATE Peladas SET status = 'closed', closed_at = ? WHERE id = ?" old-date pelada-id])
+
+            (jdbc/execute! db ["INSERT INTO peladaattendance (pelada_id, player_id, status, voting_enabled) VALUES (?, ?, 'confirmed', 1)" pelada-id target-player-id])
+            (jdbc/execute! db ["INSERT INTO peladaattendance (pelada_id, player_id, status, voting_enabled) VALUES (?, ?, 'confirmed', 1)" pelada-id voter-player-id])
+
                   ;; 4. Add a vote for the target player (e.g., 1 star to drastically change grade)
-                  (db.vote/insert-vote {:pelada-id pelada-id :voter-id voter-player-id :target-id target-player-id :stars 1} db)
+            (db.vote/insert-vote {:pelada-id pelada-id :voter-id voter-player-id :target-id target-player-id :stars 1} db)
 
                   ;; Ensure reminder doesn't exist yet
-                  (is (nil? (db.reminder/get-last-reminder-at pelada-id "vote_ended" db)))
-                  
+            (is (nil? (db.reminder/get-last-reminder-at pelada-id "vote_ended" db)))
+
                   ;; Ensure initial grade is 5.0
-                  (is (= 5.0 (:grade (db.player/get-player target-player-id db))))
+            (is (= 5.0 (:grade (db.player/get-player target-player-id db))))
 
                   ;; 5. Run scheduler
-                  (scheduler/execute-tasks! db)
+            (scheduler/execute-tasks! db)
 
                   ;; 6. Verify grade was updated
-                  (let [new-grade (:grade (db.player/get-player target-player-id db))]
-                    (is (not= 5.0 new-grade) "Grade should have been recalculated based on the vote"))
+            (let [new-grade (:grade (db.player/get-player target-player-id db))]
+              (is (not= 5.0 new-grade) "Grade should have been recalculated based on the vote"))
 
                   ;; 7. Verify reminder was inserted to prevent infinite loops
-                  (is (some? (db.reminder/get-last-reminder-at pelada-id "vote_ended" db))
-                      "PeladaReminder 'vote_ended' should have been inserted even with WAHA disabled")))))))
+            (is (some? (db.reminder/get-last-reminder-at pelada-id "vote_ended" db))
+                "PeladaReminder 'vote_ended' should have been inserted even with WAHA disabled")))))))
