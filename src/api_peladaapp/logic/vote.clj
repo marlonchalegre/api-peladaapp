895f9b4fd66aaed4ebb42d89e7f56e0311d2c7fb
Marlon Chalegre <marlonchalegre@gmail.com>
2026-05-13 23:53:55 -0300

    refactor(logic): remove parse-any-date gambiarra and use helper.time/->instant

--- src/api_peladaapp/logic/vote.clj
+++ src/api_peladaapp/logic/vote.clj
@@ -1,7 +1,6 @@
 (ns api-peladaapp.logic.vote
   (:require
-   [clojure.string :as str]
-   [clojure.tools.logging :as log])
+   [api-peladaapp.helpers.time :as time])
   (:import
    [java.time Duration Instant]))
 
@@ -27,24 +26,6 @@
                     {:type :bad-request
                      :message "Voting is only allowed after pelada is closed"}))))
 
-(defn- parse-any-date [d]
-  (cond
-    (instance? Instant d) d
-    (instance? java.time.OffsetDateTime d) (.toInstant d)
-    (instance? java.time.LocalDateTime d) (.toInstant (.atZone d (java.time.ZoneId/systemDefault)))
-    (instance? java.sql.Timestamp d) (.toInstant d)
-    (string? d) (let [s (str/replace d " " "T")
-                      s (if (re-find #"T\d{2}:\d{2}:\d{2}$" s) (str s ".000") s)
-                      s (if (str/ends-with? s "Z") s (str s "Z"))]
-                  (try (Instant/parse s)
-                       (catch Exception _
-                         (try (Instant/parse d)
-                              (catch Exception _
-                                (log/error "Failed to parse date:" d)
-                                (Instant/now))))))
-    :else (do (log/error "Unknown date type:" (type d) "value:" d)
-              (Instant/now))))
-
 (defn ensure-voting-window-open
   "Ensure voting is within 24 hours after pelada closed."
   [pelada]
@@ -53,7 +34,7 @@
       (throw (ex-info "Pelada has no closed_at timestamp"
                       {:type :bad-request
                        :message "Cannot determine voting window"})))
-    (let [closed-instant (parse-any-date closed-at)
+    (let [closed-instant (time/->instant closed-at)
           now (Instant/now)
           duration (Duration/between closed-instant now)
           limit (Duration/ofHours 24)]
